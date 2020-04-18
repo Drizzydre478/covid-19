2576090c1ac7947800d52a0f87376188310d6622
Dre <deandresmelter@gmail.com>
2020-04-17 21:13:44 -0400

    this is the last one i promise

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'public';
+export class AppComponent implements OnInit {
+  title = 'Tracking Covid-19';
+
+  constructor(@Inject(DOCUMENT) document) {}
+
+  ngOnInit() {}
+
+@HostListener('window:scroll', ['$event'])
+  onWindowScroll(e) {
+      if (window.pageYOffset > 100) {
+        let element = document.getElementsByClassName('header') as HTMLCollectionOf<HTMLElement>;
+          for(let i in element){
+            element[i].style.backgroundColor = "rgb(88, 88, 88)";
+          }
+      }
+      else {
+        let element = document.getElementsByClassName('header') as HTMLCollectionOf<HTMLElement>;
+        for(let i in element){
+          element[i].style.backgroundColor = "rgba(0, 0, 28, 0.509)";
+        }
+      }
+    }
 }
